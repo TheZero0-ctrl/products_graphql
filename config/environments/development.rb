eab5e5dea442a5697b2f143218890678830f16c4
TheZero0-ctrl <ankitpariyar486@gmail.com>
2023-06-26 22:35:58 +0545

    set up graphql and graphiql

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.session_store :cookie_store, key: '_graphiql_session'
+  config.middleware.use ActionDispatch::Cookies
+  config.middleware.use config.session_store, config.session_options
 end
